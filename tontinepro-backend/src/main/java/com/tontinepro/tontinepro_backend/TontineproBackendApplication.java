28ed16f2961fddba78c09108016efbae19875e45
Jumpy <poudjoumrodrigue@gmail.com>
2026-05-18 14:48:12 +0100

    feat: initial project structure -Spring Boot + Angular

---
+++ tontinepro-backend/src/main/java/com/tontinepro/tontinepro_backend/TontineproBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tontinepro.tontinepro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontineproBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TontineproBackendApplication.class, args);
+	}
+
+}
---
+++ tontinepro-backend/src/test/java/com/tontinepro/tontinepro_backend/TontineproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tontinepro.tontinepro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontineproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
